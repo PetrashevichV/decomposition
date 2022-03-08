9d130031d859b4b3eddcfcdcc0d60b5cf0bf81bb
Владимир <99326165+PetrashevichV@users.noreply.github.com>
2022-03-09 00:50:25 +0300

    Add files via upload

---
+++ src/cezar/pt/ch/vl/Cezar.java
@@ -0,0 +1,40 @@
+package cezar.pt.ch.vl;
+
+import java.util.Scanner;
+
+public class Cezar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = "MVEZ MZUZ MZTZ";
+		int key = 17;
+		Decrypt(s, key);
+	}
+
+	public static void Decrypt(String str, int n) {
+
+		int k = Integer.parseInt("-" + n);
+		String string = "";
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c >= 'a' && c <= 'z') {
+				c += k % 26;
+				if (c < 'a')
+					c += 26;
+				if (c > 'z')
+					c -= 26;
+			} else if (c >= 'A' && c <= 'Z') {
+				c += k % 26;
+				if (c < 'A')
+					c += 26;
+				if (c > 'Z')
+					c -= 26;
+			}
+			string += c;
+		   }
+		System.out.println("Расшифровка : " + string);
+		}
+	}
+		
+
+
